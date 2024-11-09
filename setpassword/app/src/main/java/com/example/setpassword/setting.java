e8659088e8fa3a39383b1da49dd12cf81aae7b71
tien22521469 <tiennguyenz2004@gmail.com>
2024-11-09 08:55:56 +0700

    setting

---
+++ setpassword/app/src/main/java/com/example/setpassword/setting.java
@@ -0,0 +1,12 @@
+package com.example.setpassword;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class setting extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
